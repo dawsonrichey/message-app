4a4c436163196fa8bb089e8223ee5484757f13e5
dawsonrichey <dawsonrichey89@gmail.com>
2017-10-19 15:24:24 -0400

    still working

--- frontend/src/app/messages.component.ts
+++ frontend/src/app/messages.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component'
+    template: `
+    <div *ngFor="let message of messages">
+        {{message.text}} by: {{message.owner}}
+    </div>
+    `
 })
 export class MessagesComponent {
-
+    messages = [{text:'some text', owner: 'Tim'}, {text: 'other message', owner: 'Jane'}];
 }
